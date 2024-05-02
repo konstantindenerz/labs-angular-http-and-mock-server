d0bfc7306b179902dc86a6b9e253fde3a3cd34fb
Konstantin Denerz <konstantin.denerz@gmail.com>
2024-05-02 14:10:30 +0200

    Initial commit

---
+++ apps/labs-angular-http-and-mock-server-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run labs-angular-http-and-mock-server:serve:development',
+        production: 'nx run labs-angular-http-and-mock-server:serve:production',
+      },
+      ciWebServerCommand:
+        'nx run labs-angular-http-and-mock-server:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
